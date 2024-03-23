4847d3f8c6bf870ef62687cb84a794f4757153b8
UnderWaterWelder27 <vvlad1175@gmail.com>
2024-03-23 23:44:28 +0200

    task 2: add string for current/choosen directory

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     /* 
     Task 1: Prompt the user for the blog post title, author, and category.
+    
     Resources:
         https://www.geeksforgeeks.org/how-to-execute-php-code-using-command-line/
         https://www.w3schools.com/php/php_file_open.asp#:~:text=PHP%20Read%20Single%20Line%20%2D%20fgets,single%20line%20from%20a%20file.
@@ -10,5 +11,18 @@
     $author = (string)readline("Author: ");
     $category = (string)readline("Category: ");
 
-    echo "$title, $author, $category";
+    /*
+    Task 2: Accept an optional command-line argument for the output directory. 
+    If no output directory is provided, use the current working directory.
+    
+    Resources:
+        https://www.php.net/manual/en/function.getcwd.php
+        https://stackoverflow.com/questions/11349151/php-check-if-empty-line-n
+    */
+
+    $output_directory = (string)readline("Output Directory: ");
+    if (trim($output_directory) == '') {
+        $output_directory = getcwd();
+    }
+    
 ?>
