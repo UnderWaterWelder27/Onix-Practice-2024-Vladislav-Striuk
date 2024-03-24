625e724cbd00ee9c6fc7174365e4795a4f0f3fd8
UnderWaterWelder27 <vvlad1175@gmail.com>
2024-03-24 12:31:29 +0200

    task 3: generated unique filename

--- index.php
+++ index.php
@@ -24,5 +24,19 @@
     if (trim($output_directory) == '') {
         $output_directory = getcwd();
     }
+    echo "Choosen directory: $output_directory \n";
+
+    /*
+    Task 3: Generate a unique filename for the blog post using the format 
+    "title-author-timestamp.md" (e.g., "my-first-post-johndoe-202304181430.md")
+    
+    Resources:
+        https://stackoverflow.com/questions/28977793/convert-spaces-to-dash-and-lowercase-with-php
+        https://www.w3schools.com/php/php_date.asp
+    */
+
+    $timestamp = date("YmdHis");
+    $filename = (str_replace(' ', '-', strtolower($title))) . '-' . (str_replace(' ', '-', strtolower($author))) . '-' . $timestamp . '.md';
+    echo "Filename: $filename \n";
     
 ?>
